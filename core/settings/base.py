1af0dded759a11aa297cd7dc5dfccde57f72739d
beksultan06 <nurlanuuulubeksultan@gmail.com>
2025-06-23 17:05:43 +0600

    end commit

--- core/settings/base.py
+++ core/settings/base.py
@@ -2,26 +2,44 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from django.utils.translation import gettext_lazy as _
-from core.settings.jazzmin import JAZZMIN_SETTINGS
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ALLOWED_HOSTS = ['*']
+def get_list(env_name):
+    val = os.environ.get(env_name, "")
+    return [x.strip() for x in val.split(",") if x.strip()]
 
-if DEBUG:
-    try:
-        from .development import *
-    except ImportError:
-        raise ImportError("Файл development.py не найден")
-else:
-    try:
-        from .production import *
-    except ImportError:
-        raise ImportError("Файл production.py не найден")
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key") 
+
+PRODUCTION = os.environ.get("PRODUCTION", "False") == "True"
+DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
+
+ALLOWED_HOSTS = get_list("ALLOWED_HOSTS") or ['*'] 
+
+LANGUAGES = [
+    ('ru', _('Russian')),
+    ('ky', _('Kyrgyz')),
+    ('en', _('English')),
+    ('tr', _('Turkish')),
+    ('ar', _('Arabic')),
+]
+
+LANGUAGE_CODE = "ru"
+TIME_ZONE = "Asia/Bishkek"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'ru'
+MODELTRANSLATION_LANGUAGES = ('ru', 'ky', 'tr', 'ar', 'en')
+TRANSLATABLE_MODEL_MODULES = ['app.base.models']
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 THEME_APPS = [
     "jazzmin",
@@ -47,10 +65,7 @@ MY_APPS = [
     "app.education",
     "app.applicants",
     "app.search",
-    # 'app.Getrequest',
 ]
-
-
 
 LIBRARY_APPS = [
     'modeltranslation',
@@ -60,29 +75,6 @@ LIBRARY_APPS = [
     'ckeditor',
     'django_ckeditor_5',
     'django_filters',
-    
-]
-
-LANGUAGES = [
-    ('ru', _('Russian')),
-    ('ky', _('Kyrgyz')),
-    ('en', _('English')),
-    ('tr', _('Turkish')),
-    ('ar', _('Arabic')),
-]
-
-
-MODELTRANSLATION_DEFAULT_LANGUAGE = 'ru'
-
-
-MODELTRANSLATION_LANGUAGES = ('ru', 'ky', 'tr', 'ar', 'en')
-
-TRANSLATABLE_MODEL_MODULES = ['app.base.models']
-
-
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR, 'locale'),
-    os.path.join(BASE_DIR, 'locale'),
 ]
 
 INSTALLED_APPS = [
@@ -92,52 +84,28 @@ INSTALLED_APPS = [
     *LIBRARY_APPS,
 ]
 
-CKEDITOR_UPLOAD_PATH = "uploads/"
-CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'  # URL to jQuery
-CKEDITOR_IMAGE_BACKEND = "pillow"  # Путь к пакету Pillow для обработки изображений
-CKEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': 'Custom',  # Вы можете настроить свою собственную панель инструментов CKEditor
-        'height': 300,
-        'width': 800,
-    },
-}
-# base.py
-# Разрешить все источники
-CORS_ALLOW_ALL_ORIGINS = True
-
-# Разрешенные методы
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
-
-# Разрешенные заголовки
-CORS_ALLOW_HEADERS = ["*"]
-
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = get_list("CORS_ALLOWED_ORIGINS")  #
+CORS_ALLOW_ALL_ORIGINS = os.environ.get("CORS_ALLOW_ALL_ORIGINS", "False") == "True"
+
+CSRF_TRUSTED_ORIGINS = get_list("CSRF_TRUSTED_ORIGINS")  
+
+
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
-
-ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
@@ -155,9 +123,39 @@ TEMPLATES = [
     },
 ]
 
-
-
 WSGI_APPLICATION = "core.wsgi.application"
+
+STATIC_URL = '/back_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'back_static')
+
+MEDIA_URL = "back_media/"
+MEDIA_ROOT = BASE_DIR / "back_media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+from core.settings.jazzmin import JAZZMIN_SETTINGS
+JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
+CKEDITOR_IMAGE_BACKEND = "pillow"
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'height': 300,
+        'width': 800,
+    },
+}
+CKEDITOR_5_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height': 300,
+        'width': '100%',
+        'config': {
+            'language': 'en',
+        },
+    },
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -173,37 +171,3 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
-LANGUAGE_CODE = "ru"
-
-TIME_ZONE = "Asia/Bishkek"
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-STATIC_URL = '/back_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'back_static')
-
-
-MEDIA_URL = "back_media/"
-MEDIA_ROOT = BASE_DIR/"back_media"
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-JAZZMIN_SETTINGS=JAZZMIN_SETTINGS
-AUTH_USER_MODEL = "auth.User"
-
-CKEDITOR_5_CONFIGS = {
-    'default': {
-        'toolbar': 'full',
-        'height': 300,
-        'width': '100%',
-        'config': {
-            'language': 'en',
-        },
-    },
-}
